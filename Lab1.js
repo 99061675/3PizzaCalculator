cd5f3d2293ac3701e0b2c7c1ad71b80534ec87db
MathieuKers <9961675@mydavinci.nl>
2020-09-07 12:36:53 +0200

    My first commit

---
+++ Lab1.js
@@ -0,0 +1,8 @@
+// begin hier je JavaScript commandos
+alert('Hallo u gaat 3 vragen beantwoorden');
+
+document.write(prompt('Voer uw Voornaam in.'));
+
+document.write(prompt('Voer uw Achternaam in.'));
+
+document.write(prompt('Voer uw Leeftijd in.'));
