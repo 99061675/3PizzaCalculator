e43abebbdaac4e0056be835f894b55ea802e3727
MathieuKers <9961675@mydavinci.nl>
2020-09-09 13:57:38 +0200

    My first commit

---
+++ Lab2.js
@@ -0,0 +1,11 @@
+//Mathieu Kers
+//Software Developer
+const STOPGETAL = alert('Hallo u gaat 3 vragen beantwoorden');//Geeft een melding op je scherm.
+
+var voornaam = prompt('Voer uw voornaam in');//Slaat de gegevens die je in de vraag invoert op.
+var achternaam = prompt('Voer uw achternaam in');//Slaat de gegevens die je in de vraag invoert op.
+var leeftijd = prompt('Voer uw leeftijd in');//Slaat de gegevens die je in de vraag invoert op.
+
+document.write(voornaam);//Schrijft het in het document.
+document.write(achternaam);//Schrijft het in het document.
+document.write(leeftijd);//Schrijft het in het document.
---
+++ Lab3.js
@@ -0,0 +1,65 @@
+//De tafel van 6
+const GETAL = 6;
+document.write('De tafel van 6');
+document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+document.write('<br>10 x ' + GETAL + ' = ' + 10 * GETAL);
+
+document.write('<br>');
+
+//De tafel van 3
+const getal = 3;
+document.write('<br>De tafel van 3');
+document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+document.write('<br>10 x ' + getal + ' = ' + 10 * getal);
+
+document.write('<br>');
+
+//De som
+document.write('<br>De som');
+var cijfer = 12;
+document.write('<br>' + cijfer + ' + 6 =');
+cijfer = cijfer + 6;
+document.write(cijfer);
+document.write('<br>' + cijfer + ' x 10 =');
+cijfer = cijfer * 10;
+document.write(cijfer);
+document.write('<br>' + cijfer + ' : 5 =');
+cijfer = cijfer / 5;
+document.write(cijfer);
+document.write('<br>' + cijfer + ' - 12 =');
+cijfer = cijfer - 12;
+document.write(cijfer);
+
+document.write('<br>')
+
+//De som zelf invullen
+document.write('<br>De som zelf invullen');
+antwoord=parseInt(prompt('Voer een getal in:'));
+document.write('<br>' + antwoord + ' + 6 =');
+antwoord=antwoord + 6;
+document.write(antwoord)
+document.write('<br>' + antwoord + ' x 10 =');
+antwoord=antwoord * 10;
+document.write(antwoord)
+document.write('<br>' + antwoord + ' : 5 =');
+antwoord=antwoord / 5;
+document.write(antwoord)
+document.write('<br>' + antwoord + ' - 12 =');
+antwoord=antwoord - 12;
+document.write(antwoord)
